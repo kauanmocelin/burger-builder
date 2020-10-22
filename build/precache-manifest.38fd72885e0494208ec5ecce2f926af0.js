911c8c71ec18079502cbc7840a59e012802489ac
kauanmocelin <kauanmocelin@gmail.com>
2020-10-22 15:55:35 -0300

    Adicionado ao git o diretório build na finalidade de rodar action do github para deploy

---
+++ build/precache-manifest.38fd72885e0494208ec5ecce2f926af0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e64084c492e8ac605c282de68e2741",
+    "url": "/index.html"
+  },
+  {
+    "revision": "990999249cc7dc996732",
+    "url": "/static/css/3.7c8277d1.chunk.css"
+  },
+  {
+    "revision": "e9f9919c92c37cf9228b",
+    "url": "/static/css/4.e102e8ca.chunk.css"
+  },
+  {
+    "revision": "5b36bd27f720912aa013",
+    "url": "/static/css/5.0c143387.chunk.css"
+  },
+  {
+    "revision": "9742459f73443624f030",
+    "url": "/static/css/main.fa08f39f.chunk.css"
+  },
+  {
+    "revision": "7c95873d60eab69eba43",
+    "url": "/static/js/2.2e40b269.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2e40b269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990999249cc7dc996732",
+    "url": "/static/js/3.d0db7f50.chunk.js"
+  },
+  {
+    "revision": "e9f9919c92c37cf9228b",
+    "url": "/static/js/4.f67c61a5.chunk.js"
+  },
+  {
+    "revision": "5b36bd27f720912aa013",
+    "url": "/static/js/5.32a34b3e.chunk.js"
+  },
+  {
+    "revision": "9742459f73443624f030",
+    "url": "/static/js/main.079662f9.chunk.js"
+  },
+  {
+    "revision": "3eb537c54d598811205c",
+    "url": "/static/js/runtime-main.8f4508b5.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
